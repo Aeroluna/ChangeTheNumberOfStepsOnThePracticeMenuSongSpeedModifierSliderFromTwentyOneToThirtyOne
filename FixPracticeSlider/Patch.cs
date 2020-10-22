6288c24269a81935f6558065cc88f591441dbfac
Aeroluna <aeroluna5@gmail.com>
2020-10-22 13:04:09 -0700

    Add project files.

---
+++ FixPracticeSlider/Patch.cs
@@ -0,0 +1,16 @@
+﻿namespace ChangeTheNumberOfStepsOnThePracticeMenuSongSpeedModifierSliderFromTwentyOneToThirtyOne
+{
+    using HarmonyLib;
+    using HMUI;
+
+    [HarmonyPatch(typeof(PracticeViewController))]
+    [HarmonyPatch("Init")]
+    internal static class PracticeViewControllerInit
+    {
+        private static void Postfix(PercentSlider ____speedSlider)
+        {
+            // 21 => 31
+            ____speedSlider.numberOfSteps = 31;
+        }
+    }
+}
---
+++ FixPracticeSlider/Plugin.cs
@@ -0,0 +1,19 @@
+﻿namespace ChangeTheNumberOfStepsOnThePracticeMenuSongSpeedModifierSliderFromTwentyOneToThirtyOne
+{
+    using System.Reflection;
+    using HarmonyLib;
+    using IPA;
+
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    internal class Plugin
+    {
+        private const string HARMONYID = "com.noodle.BeatSaber.FixPracticeSlider";
+        private static readonly Harmony _harmonyInstance = new Harmony(HARMONYID);
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            _harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
